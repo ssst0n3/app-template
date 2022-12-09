80f659e4af65ae98c95ad4cf3701deb194a254c5
st0n3 <ssst0n3@gmail.com>
2022-12-09 23:11:28 +0800

    downgrade swaggo to v1.8.7 instead of v1.8.8 to fix #2

--- hack/swaggo.sh
+++ hack/swaggo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-GO111MODULE="on" GOPROXY=$GOPROXY go install github.com/swaggo/swag/cmd/swag@latest
+GO111MODULE="on" GOPROXY=$GOPROXY go install github.com/swaggo/swag/cmd/swag@v1.8.7
 cd $(dirname $(dirname $(readlink -f $0)))
 cd cmd/app
 swag init --parseDependency --parseInternal -o ../../docs
